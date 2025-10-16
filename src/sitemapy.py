3b16aab325d3cb2970809ca1433bdf4419cc1200
HamBoneGS <graham.n.schmidt@gmail.com>
2025-10-16 14:36:01 -0500

    create URLEntry object

--- src/sitemapy.py
+++ src/sitemapy.py
@@ -1,15 +1,36 @@
 from datetime import datetime
 import xml.etree.ElementTree as ET
 
+
+class URLEntry:
+    def __init__(self, full_url: str):
+        self.full_url = full_url
+        self.lastmod = ""
+
+
 class Sitemap:
     def __init__(self):
-        pass
-        
-    def write_sitemap_to_file(self, input_urls: list[str], output_filename: str = None, lastmod_now: bool = False):
-        """Write a sitemap XML file.
+        self.output_filename: str = ""
+        self.urls: list[URLEntry] = []
+
+    def load_from_list(self, urls: list[str]):
+        """Builds basic sitemap from list of URLs, with no additonal attributes"""
+        if len(urls) < 1:
+            return ValueError("URL list must contain at least 1 URL")
+
+        for url in urls:
+            entry = URLEntry(
+                full_url=url,
+            )
+            self.urls.append(entry)
+
+    # proposal - turn into `build_from_list()`
+    def write_sitemap_to_file(
+        self, output_filename: str = None, lastmod_now: bool = False
+    ):
+        """Write a sitemap XML file from current instance.
 
         Args:
-            input_urls (list[str]): A list of URLs to pupulate the sitemap with
             output_filename (str) [Optional]: The desired name of the XML file. Default = "sitemap.xml
             lastmod_now (bool): Create lastmod attribute per URL, set to today's date. Default = False
 
@@ -17,17 +38,17 @@ class Sitemap:
             bool: Operation success
         """
         try:
-            if len(input_urls) < 1:
-                return ValueError("List of URLs must have at least one value")
-
             if not output_filename:
                 output_filename = "sitemap.xml"
 
-            root = ET.Element('urlset', xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+            root = ET.Element(
+                "urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            )
 
-            
-            for url in input_urls:
-                self._append_url_element(root=root, url=url, lastmod_now=lastmod_now)
+            for url_entry in self.urls:
+                self._append_url_element(
+                    root=root, url=url_entry.full_url, lastmod_now=lastmod_now
+                )
 
             tree = ET.ElementTree(root)
             ET.indent(tree, space="   ")
@@ -36,14 +57,16 @@ class Sitemap:
         except:
             return False
 
-    def _append_url_element(self, root: ET.Element, url: str, lastmod_now: bool = False):
+    def _append_url_element(
+        self, root: ET.Element, url: str, lastmod_now: bool = False
+    ):
         """Append URL element to given root element"""
-        url_elem = ET.SubElement(root, 'url')
-        loc = ET.SubElement(url_elem, 'loc')
-        
+        url_elem = ET.SubElement(root, "url")
+        loc = ET.SubElement(url_elem, "loc")
+
         if lastmod_now:
-            lastmod = ET.SubElement(url_elem, 'lastmod')
+            lastmod = ET.SubElement(url_elem, "lastmod")
             date_string = datetime.now().strftime("%Y-%m-%d")
             lastmod.text = date_string
-            
+
         loc.text = url
