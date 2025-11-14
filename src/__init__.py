d025f9b2567d83f9e2c4d1573025811e5a90ee3c
HamBoneGS <graham.n.schmidt@gmail.com>
2025-11-14 08:16:46 -0600

    badge link

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-# src/__init__.py
-from .sitemapy import Sitemap, URLEntry, HreflangAlternate, SitemapIndex, IndexEntry
-
-__all__ = ["Sitemap", "URLEntry", "HreflangAlternate", "SitemapIndex", "IndexEntry"]
-__version__ = "0.1.0"
